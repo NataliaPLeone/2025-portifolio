878febe029eee15901d0d61fe918239a49f2036e
Natália <nataliapl@usp.br>
2025-07-27 03:12:18 -0300

    primeiro commit teste

---
+++ app/page.tsx
@@ -0,0 +1,26 @@
+
+export default function Home() {
+  return (
+    <main className="min-h-screen md:h-screen flex flex-col bg-white dark:bg-gray-900 pb-16 sm:pb-0">
+			<div className="flex-1 grid grid-cols-1 md:grid-cols-5">
+				{/* Left Column - Profile */}
+				<div className="md:col-span-2 border-b md:border-b-0 md:border-r border-gray-200 dark:border-gray-800 md:flex md:items-center">
+					
+				</div>
+
+				{/* Right Column - Skills and Experience */}
+				<div className="md:col-span-3 flex flex-col md:justify-center">
+					{/* Skills Section */}
+					<div className="">
+						
+					</div>
+
+					{/* Experience Section */}
+					<div>
+						
+					</div>
+				</div>
+			</div>
+		</main>
+  );
+}
